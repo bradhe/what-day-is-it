987f9800d9c7f6163942776a85447bc1d4b00308
Brad Heller <brad.heller@gmail.com>
2020-04-17 16:20:30 -0700

    Deploy the new thing, not the old thing.

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,6 +2,6 @@
 
 HOST=$1
 
-GOOS=linux GOARCH=amd64 go build .
+GOOS=linux GOARCH=amd64 go build ./cmd/what-day-is-it
 scp what-day-is-it what-day-is-it:what-day-is-it
 ssh $1 'sudo mv what-day-is-it /usr/local/bin/what-day-is-it && sudo service what-day-is-it restart'
