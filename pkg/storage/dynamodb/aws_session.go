23733fe6b1a2d469b8e1c21442543ce57d0c8169
Brad Heller <brad.heller@gmail.com>
2020-04-23 11:28:07 -0700

    Just use default behavior for AWS configuration

--- pkg/storage/dynamodb/aws_session.go
+++ pkg/storage/dynamodb/aws_session.go
@@ -1,60 +1,10 @@
 package dynamodb
 
 import (
-	"os"
-
-	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/aws/credentials"
-	"github.com/aws/aws-sdk-go/aws/credentials/ec2rolecreds"
-	"github.com/aws/aws-sdk-go/aws/ec2metadata"
+	"github.com/aws/aws-sdk-go/aws/defaults"
 	"github.com/aws/aws-sdk-go/aws/session"
 )
 
-const DefaultAWSRegion = "us-east-1"
-
-var (
-	// Default (mostly unconfigured) session to use with the default metadata client.
-	defaultSession = session.Must(session.NewSession(aws.NewConfig()))
-
-	// Default metadata client to use when fetching the default credentials.
-	defaultMetadataClient = ec2metadata.New(defaultSession)
-
-	defaultCredentials = credentials.NewChainCredentials(
-		[]credentials.Provider{
-			&credentials.EnvProvider{},
-			&ec2rolecreds.EC2RoleProvider{
-				Client: defaultMetadataClient,
-			},
-		})
-)
-
-var knownRegion = ""
-
-func getAWSRegion() string {
-	// we just cache the region info so we don't have to look it up every damn
-	// time.
-	if knownRegion != "" {
-		return knownRegion
-	}
-
-	if region := os.Getenv("AWS_REGION"); region != "" {
-		return region
-	}
-
-	if region, err := defaultMetadataClient.Region(); err == nil {
-		knownRegion = DefaultAWSRegion
-		return DefaultAWSRegion
-	} else {
-		knownRegion = region
-		return region
-	}
-}
-
 func newAWSSession() *session.Session {
-	config := aws.NewConfig().
-		WithRegion(getAWSRegion()).
-		WithMaxRetries(3).
-		WithCredentials(defaultCredentials)
-
-	return session.Must(session.NewSession(config))
+	return session.Must(session.NewSession(defaults.Get().Config))
 }
