a77ce5fee3d2f9ec418c57ee225616e8732358a6
Brad Heller <brad.heller@gmail.com>
2020-04-23 17:34:17 -0700

    Don't need this anymore.

--- scripts/deploy.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-HOST=$1
-
-GOOS=linux GOARCH=amd64 go build ./cmd/what-day-is-it
-scp what-day-is-it what-day-is-it:what-day-is-it
-ssh $1 'sudo mv what-day-is-it /usr/local/bin/what-day-is-it && sudo service what-day-is-it restart'
